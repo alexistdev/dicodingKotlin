b4b45b94529ccd6070414ed77b7fc06f88f6de81
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2022-06-15 05:52:21 +0700

    Latihan kotlin functional programming 15 juni

---
+++ FunctionalProgramming/src/main/kotlin/konsep/Extension.kt
@@ -0,0 +1,5 @@
+package konsep
+
+fun main(){
+    
+}
--- Kotlin-starter/src/main/kotlin/Collection/CollectionOperations.kt
+++ Kotlin-starter/src/main/kotlin/Collection/CollectionOperations.kt
@@ -17,11 +17,29 @@ fun main(){
 //    println(numberList.count{ it % 2 == 0})
 
     /** find(), firstOrNull(), lastOrNull */
-    val numberList =  listOf(1,2,3,4,5)
-    val firstOddNumber = numberList.find { it % 2 ==1}
-    val firstOrNullNumber = numberList.firstOrNull { it % 2 == 3}
+//    val numberList =  listOf(1,2,3,4,5)
+//    val firstOddNumber = numberList.find { it % 2 ==1}
+//    val firstOrNullNumber = numberList.firstOrNull { it % 2 == 3}
+//
+//    println(firstOddNumber)
+//    println(firstOrNullNumber)
+    /** first() & last() */
+//    val numberList = listOf(1,2,3,4,5,6,7,8,9,10)
+//    val moreThan10 = numberList.first{ it > 9}
+//    print(moreThan10)
+    /** sum() */
+//    val numberList = listOf(1,2,3,4,5,6,7,8,9,10)
+//    val total = numberList.sum()
+//    println(total)
+    /** sorted() = mengurutkan data di dalam collection secara ascending */
+//    val kotlinChar = listOf('e','f','c','b','a','d')
+//    val ascendingSort =  kotlinChar.sorted()
+//
+//    println(ascendingSort)
+    /** sortedDescending() */
+    val kotlinChar = listOf('a','b','c')
+    val descendingSort = kotlinChar.sortedDescending()
 
-    println(firstOddNumber)
-    println(firstOrNullNumber)
+    println(descendingSort)
 
 }
